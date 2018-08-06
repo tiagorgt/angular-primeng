a6b36ff24b6fc17df01fe84b78168c982e680a81
Tiago Ranghetti <tiago.ranghetti@senior.com.br>
2018-08-06 18:58:53 -0300

    Fix object type

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ItemService } from './item.service';
 import { Item } from '../model/item';
@@ -160,7 +160,7 @@ export class ItemComponent implements OnInit {
     return false;
   }
 
-  isFormControlValid(formControl: FormControl) {
+  isFormControlValid(formControl: AbstractControl) {
     return !formControl.valid && (formControl.dirty || this.formSubmitted);
   }
 
